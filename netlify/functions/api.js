c9b2c95aac1cb1a685091fcda0f4c497a9604827
Habiba Hassan <habiba.hassaneen@gmail.com>
2025-03-16 01:33:54 +0200

    fix

--- netlify/functions/api.js
+++
@@ -1,8 +0,0 @@
-const { builder } = require("@netlify/functions");
-const { createRequestHandler } = require("@netlify/next");
-
-const handler = createRequestHandler({
-  compression: false,
-});
-
-exports.handler = builder(handler);
