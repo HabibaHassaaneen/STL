f2ebe8379298bf8afaf31a44d12a1838e5e23192
Habiba Hassan <habiba.hassaneen@gmail.com>
2025-03-15 19:58:40 +0200

    last

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,20 +1,18 @@
-import type { Metadata } from "next";
+
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Provider from "./Provider"
 
 const inter = Inter({ subsets: ["latin"] });
 
-export const metadata: Metadata = {
+export const metadata = {
   title: "Lama Dev School Management Dashboard",
   description: "Next.js School Management System",
 };
 
 export default function RootLayout({
   children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+} ) {
   return (
     <html lang="en">
       <body className={inter.className}>
